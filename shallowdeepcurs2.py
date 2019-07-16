b6a0a8e495b967a09d7d8714d04be940f6168380
Pandele George <pandelegeorge@telacad.ro>
2019-07-16 20:54:34 +0300

    Python curs2 advanced first commit

---
+++ shallowdeepcurs2.py
@@ -0,0 +1,20 @@
+import copy
+
+a = [[1, 2, 3], [4, 5, 6]]
+b = a
+print("a and b before modify")
+print(a)
+print(b)
+a[0][1] = 10
+print("a and b after modfiy. b was obtain as a copy of a")
+print(a)
+print(b)
+
+b = copy.deepcopy(a)
+print("a nad b before modify")
+print(a)
+print(b)
+a[0][1] = 9
+print("a and b after modify")
+print(a)
+print(b)
